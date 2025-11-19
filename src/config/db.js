4b8340741c2c0cbcf40c180cb9b8f4c8e59a3b2e
Diego Scopetta <diegooscopetta@gmail.com>
2025-11-19 10:35:02 -0300

    Últimos ajustes del proyecto

--- src/config/db.js
+++ src/config/db.js
@@ -5,8 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    console.log("👉 URI desde .env:", process.env.MONGO_URI); 
-
+  
     await mongoose.connect(process.env.MONGO_URI);
     console.log("✅ Conectado correctamente a MongoDB");
   } catch (error) {
